4ada86d173224a63d1d92dc96db2163ed83f5d1d
mani771998 <smanjot432@gmail.com>
2026-02-07 17:07:27 +0530

    Update insertdate.sql

--- scripts/insertdate.sql
+++ scripts/insertdate.sql
@@ -7,82 +7,86 @@ The data is loaded from the csv files into the corresponding tables in the bronz
 ==================================================================
 */
 
-truncate table bronze.crm_cust_info;
+-- Create stored procedure to load data into bronze tables. This will improvde efficiency and maintainability of the data loading process. The stored procedure can be scheduled to run at regular intervals to ensure that the data in the bronze layer is always up to date.
 
---Truncate is used to delete all the data from the table before loading new data. This ensures that there are no duplicate records in the table after the bulk insert operation.
+Create or alter procedure sp_load_bronze_data as
+	begin
 
-Bulk Insert bronze.crm_cust_info 
-from 'C:\Users\biker\OneDrive\Desktop\sql-data-warehouse-project-main\sql-data-warehouse-project-main\datasets\source_crm\cust_info.csv'
-with
-(
-	FIELDTERMINATOR = ',',
-	TABLOCK,
-	FIRSTROW = 2
-);
+	truncate table bronze.crm_cust_info;
 
-go
+	--Truncate is used to delete all the data from the table before loading new data. This ensures that there are no duplicate records in the table after the bulk insert operation.
 
-Truncate table bronze.crm_prd_info;
+	Bulk Insert bronze.crm_cust_info 
+	from 'C:\Users\biker\OneDrive\Desktop\sql-data-warehouse-project-main\sql-data-warehouse-project-main\datasets\source_crm\cust_info.csv'
+	with
+	(
+		FIELDTERMINATOR = ',',
+		TABLOCK,
+		FIRSTROW = 2
+	);
 
-bulk insert bronze.crm_prd_info
-from 'C:\Users\biker\OneDrive\Desktop\sql-data-warehouse-project-main\sql-data-warehouse-project-main\datasets\source_crm\prd_info.csv'
-with
-(
-FIRSTROW = 2,
-Fieldterminator = ',',
-Tablock
-);
+	go
 
-go
+	Truncate table bronze.crm_prd_info;
 
-Truncate table bronze.crm_sales_details;
+	bulk insert bronze.crm_prd_info
+	from 'C:\Users\biker\OneDrive\Desktop\sql-data-warehouse-project-main\sql-data-warehouse-project-main\datasets\source_crm\prd_info.csv'
+	with
+	(
+	FIRSTROW = 2,
+	Fieldterminator = ',',
+	Tablock
+	);
 
-bulk insert bronze.crm_sales_details
-from 'C:\Users\biker\OneDrive\Desktop\sql-data-warehouse-project-main\sql-data-warehouse-project-main\datasets\source_crm\sales_details.csv'
-with
-(Firstrow = 2,
-fieldterminator = ',',
-tablock
-);
+	go
 
-go
+	Truncate table bronze.crm_sales_details;
 
-Truncate table bronze.erp_cust_AZ12;
+	bulk insert bronze.crm_sales_details
+	from 'C:\Users\biker\OneDrive\Desktop\sql-data-warehouse-project-main\sql-data-warehouse-project-main\datasets\source_crm\sales_details.csv'
+	with
+	(Firstrow = 2,
+	fieldterminator = ',',
+	tablock
+	);
 
-bulk insert bronze.erp_cust_AZ12
-from 'C:\Users\biker\OneDrive\Desktop\sql-data-warehouse-project-main\sql-data-warehouse-project-main\datasets\source_erp\CUST_AZ12.csv'
-with 
-(
-firstrow = 2,
-tablock,
-fieldterminator = ','
-);
+	go
 
-go
+	Truncate table bronze.erp_cust_AZ12;
 
-Truncate table bronze.erp_LOC_A101;
-
-bulk insert bronze.erp_LOC_A101
-
-from 'C:\Users\biker\OneDrive\Desktop\sql-data-warehouse-project-main\sql-data-warehouse-project-main\datasets\source_erp\LOC_A101.csv'
-with 
-(
-firstrow = 2,
-tablock,
-fieldterminator = ','
-);
-
-go
-
-Truncate table bronze.erp_px_cat_g1v2;
-
-bulk insert bronze.erp_px_cat_g1v2
-from 'C:\Users\biker\OneDrive\Desktop\sql-data-warehouse-project-main\sql-data-warehouse-project-main\datasets\source_erp\PX_CAT_G1V2.csv'
-with 
-(
+	bulk insert bronze.erp_cust_AZ12
+	from 'C:\Users\biker\OneDrive\Desktop\sql-data-warehouse-project-main\sql-data-warehouse-project-main\datasets\source_erp\CUST_AZ12.csv'
+	with 
+	(
 	firstrow = 2,
 	tablock,
 	fieldterminator = ','
-);
+	);
 
+	go
 
+	Truncate table bronze.erp_LOC_A101;
+
+	bulk insert bronze.erp_LOC_A101
+
+	from 'C:\Users\biker\OneDrive\Desktop\sql-data-warehouse-project-main\sql-data-warehouse-project-main\datasets\source_erp\LOC_A101.csv'
+	with 
+	(
+	firstrow = 2,
+	tablock,
+	fieldterminator = ','
+	);
+
+	go
+
+	Truncate table bronze.erp_px_cat_g1v2;
+
+	bulk insert bronze.erp_px_cat_g1v2
+	from 'C:\Users\biker\OneDrive\Desktop\sql-data-warehouse-project-main\sql-data-warehouse-project-main\datasets\source_erp\PX_CAT_G1V2.csv'
+	with 
+	(
+		firstrow = 2,
+		tablock,
+		fieldterminator = ','
+	);
+end
