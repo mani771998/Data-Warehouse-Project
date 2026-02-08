0e450710e656e502d9ce0df108e3545408447297
mani771998 <smanjot432@gmail.com>
2026-02-08 15:54:12 +0530

    Update insertdata.sql

--- scripts/silver/insertdata.sql
+++ scripts/silver/insertdata.sql
@@ -34,3 +34,35 @@ where flag_last =  1
 
 
 --------------------------------------------------------------------------------------
+
+
+--This is for silver.crm_prd_info
+
+
+insert into silver.crm_prd_info (
+    prd_id,
+	cat_id ,
+	prd_key,
+	prd_nm ,
+	prd_cost ,
+	prd_line ,
+	prd_start_dt ,
+	prd_end_dt
+)
+select prd_id,
+replace(substring(prd_key, 1, 5), '-', '_') as cat_id,
+SUBSTRING(prd_key, 7, len(prd_key)) as prd_key,
+prd_nm, 
+isnull(prd_cost, 0) as prd_cost, 
+case trim(prd_line)
+	when 'M' then 'Mountain'
+	when 'R' then 'Road'
+	when 'S' then 'Other Sales'
+	when 'T' then 'Touring'
+	else 'N/A'
+end as prd_line,
+prd_start_dt,
+dateadd(day, - 1, lead(prd_start_dt) over(partition by prd_key order by prd_start_dt)) as prd_end_dt
+from bronze.crm_prd_info
+
+
